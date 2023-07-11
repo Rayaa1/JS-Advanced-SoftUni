e8691eca4371d364d249df24a1532c6d596ca89f
Raya Koleva <117087939+Rayaa1@users.noreply.github.com>
2023-07-12 00:40:17 +0300

    Delete 03.Objects and Composition - Exercise directory

--- 03.Objects and Composition - Exercise/01.calorieObject.js
+++
@@ -1,8 +0,0 @@
-function calorieObject(array) {
-	let result = {};
-
-	for (let i = 0; i < array.length; i += 2) {
-		result[array[i]] = Number(array[i + 1]);
-	}
-	console.log(result);
-}
--- 03.Objects and Composition - Exercise/02.constructionCrew.js
+++
@@ -1,7 +0,0 @@
-function constructionCrew(obj) {
-	if (obj.dizziness === true) {
-		obj.levelOfHydrated += 0.1 * obj.weight * obj.experience;
-		obj.dizziness = false;
-	}
-	return obj;
-}
