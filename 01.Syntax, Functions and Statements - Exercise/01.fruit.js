64f73a4dac2e54cd77304b8cb6469483d34b3f2e
Raya Koleva <117087939+Rayaa1@users.noreply.github.com>
2023-07-12 00:39:42 +0300

    Delete 01.Syntax, Functions and Statements - Exercise directory

--- 01.Syntax, Functions and Statements - Exercise/01.fruit.js
+++
@@ -1,7 +0,0 @@
-function fruit (product,weightGr,price){
-    let weightKg = weightGr / 1000
-    let pricePerKg = weightKg * price
-    console.log(`I need $${pricePerKg.toFixed(2)} to buy ${weightKg.toFixed(2)} kilograms ${product}.`)
-
-}
-fruit('orange', 2500, 1.80)
--- 01.Syntax, Functions and Statements - Exercise/09.wordUpperCase.js
+++
@@ -1,3 +0,0 @@
-function wordsUpperCase(text) {
-	return words = text.match(new RegExp(/\w+/g)).join(", ").toUpperCase();
-}
