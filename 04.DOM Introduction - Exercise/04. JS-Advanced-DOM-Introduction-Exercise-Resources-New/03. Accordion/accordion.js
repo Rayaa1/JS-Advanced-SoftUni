9f8d736832e9df970d933d61102fecba5b9bba8b
Raya Koleva <117087939+Rayaa1@users.noreply.github.com>
2023-07-12 00:40:29 +0300

    Delete 04.DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New directory

--- 04.DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++
@@ -1,9 +0,0 @@
-function toggle() {
-   const MORE = "More"
-   const LESS = "Less"
-   const buttonSpan = document.querySelector(".button")
-   const text = document.querySelector("#extra")
-   buttonSpan.textContent = buttonSpan.textContent === MORE ? LESS : MORE
-   text.style.display = text.style.display == 'block' ? 'none' : 'block';
-
-}
