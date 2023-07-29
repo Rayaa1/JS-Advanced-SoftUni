e28ae7e791e75cc4bbde757b398dfbf827bc1b85
Raya Koleva <117087939+Rayaa1@users.noreply.github.com>
2023-07-29 11:28:09 +0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,42 +1,76 @@
-import {render} from '../node_modules/lit-html/lit-html.js'
-import page from '../node_modules/page/page.mjs'
-import { logout } from './data/auth.js'
-import { getUserData } from './util.js'
-import { catalogPage } from './views/catalog.js'
-import { createPage } from './views/create.js'
-import { detailsPage } from './views/details.js'
-import { editPage } from './views/edit.js'
-import { homePage } from './views/home.js'
-import { layoutTemplate } from './views/layout.js'
-import { loginPage } from './views/login.js'
-import { registerPage } from './views/register.js'
-const root = document.getElementById('wrapper')
-page(decorateContext)
-//page('index.html','/')
-page('/',homePage)
-page('/login',loginPage)
-page('/register',registerPage)
-page('/logout',logoutAction)
-page('/catalog',catalogPage)
-page('/catalog/:id',detailsPage)
-page('/create',createPage)
-page('/catalog/:id/edit',editPage)
+import { render } from "../node_modules/lit-html/lit-html.js";
+import page from "../node_modules/page/page.mjs";
 
-page.start()
+import { logout as apiLogout } from "./api/api.js";
+import { getUserData } from "./utility.js";
+import { loginPage, registerPage } from "./view/auth.js";
+import { homePage } from "./view/home.js";
+import { dashboardPage } from "./view/dashboard.js";
+import { detailsPage } from "./view/details.js";
+import { editPage } from "./view/edit.js";
+import { createPage } from "./view/create.js";
 
-function decorateContext(ctx,next){
-    ctx.render = renderView
+const main = document.querySelector("#content");
 
-    next()
+setUserNav();
+
+document.getElementById("logoutBtn").addEventListener("click", onLogout);
+
+page("/", decorateContext, homePage);
+page("/login", decorateContext, loginPage);
+page("/register", decorateContext, registerPage);
+page("/dashboard", decorateContext, dashboardPage);
+page("/details/:id", decorateContext, detailsPage);
+page("/edit/:id", decorateContext, editPage);
+page("/add-event", decorateContext, createPage);
+
+page.start();
+
+function decorateContext(ctx, next) {
+  ctx.render = (content) => render(content, main);
+  ctx.setUserNav = setUserNav;
+  ctx.user = getUserData();
+
+  next();
 }
-function renderView(content){
-    const userData = getUserData()
-    render(layoutTemplate(userData,content),root)
 
+// function setUserNav() {
+//   const user = getUserData();
+//   if (user) {
+//     document
+//       .querySelectorAll(".user")
+//       .forEach((x) => (x.style.display = "inline"));
+//     document
+//       .querySelectorAll(".guest")
+//       .forEach((x) => (x.style.display = "none"));
+//   } else {
+//     document
+//       .querySelectorAll(".user")
+//       .forEach((x) => (x.style.display = "none"));
+//     document
+//       .querySelectorAll(".guest")
+//       .forEach((x) => (x.style.display = "inline"));
+//   }
+// }
+
+function setUserNav() {
+  const userAsJson = sessionStorage.getItem("user");
+  const user = userAsJson && JSON.parse(userAsJson);
+  const guestDiv = document.querySelector(".guest");
+  const userDiv = document.querySelector(".user");
+  user != null
+    ? [
+        (userDiv.style.display = "inline-block"),
+        (guestDiv.style.display = "none"),
+      ]
+    : [
+        (userDiv.style.display = "none"),
+        (guestDiv.style.display = "inline-block"),
+      ];
 }
 
-function logoutAction(ctx){
-    logout()
-    ctx.page.redirect('/')
-
+async function onLogout() {
+  await apiLogout();
+  setUserNav();
+  page.redirect("/");
 }
